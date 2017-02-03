bbd940cabd5d6d123bdbef5b49e97d02a50ea109
margogunatama <margogunatama@gmail.com>
2017-02-03 09:59:38 +0700

    PHPnya
    dicek lagi takut bener hehe

---
+++ Modul PHP Mysql/footer.php
@@ -0,0 +1,7 @@
+<?php
+	echo '
+		<footer>
+			<p>Copyright &copy; 2017 | <a href="">Kontak Kami</a></p>
+		</footer>
+	';
+?>
---
+++ Modul PHP Mysql/keranjang.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Keranjang | Toko Online</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<!-- Navbar -->
+<?php
+	include 'navbar.php';
+?>
+<!-- End Navbar -->
+
+<!-- Contain -->
+
+<div class="main">
+	
+</div>
+
+<!-- End Contain -->
+
+<!-- Footer -->
+<?php
+	include 'footer.php';
+?>
+<!-- End Footer -->
+</body>
+</html>
