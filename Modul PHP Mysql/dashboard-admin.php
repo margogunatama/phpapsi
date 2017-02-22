b02a328697fb2ce1bb7735c991e9e66a8e14bb06
Qalbinuril Setyani <qalbins@gmail.com>
2017-02-22 23:24:53 +0700

    Front end - Modul PHP
    Login, DashBoard Admin

---
+++ Modul PHP Mysql/dashboard-admin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/style-admin.css">
+    </head>
+    <body>
+
+        <div class="container">
+
+            <div class="header">
+              <div class="left-bar">
+                <h2>Admin</h2>
+              </div>
+              <div class="right-bar">
+                <a href="login.html">Logout</a>
+              </div>
+            </div>
+
+            <div class="middle">
+                <div class="left-bar">
+                    <ul>
+                        <li><a class="active" href="#">Dashboard</a></li>
+                        <li><a href="#">Master Data</a></li>
+                        <li><a href="#">Log</a></li>
+                        <li><a href="#">Barang</a></li>
+                    </ul>
+                </div>
+                <div class="content">
+                  <p>Ini Halaman Admin</p>
+                </div>
+            </div>
+            <div class="clear"></div>
+            <div class="footer">
+              <h4>Copyright &copy; 2017</h4>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ Modul PHP Mysql/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+<form action="dashboard-admin.html" method="post">
+  <div class="container">
+  <div class="imgcontainer">
+    <img src="images/Logo1.png" width="100px" height="100px" alt="Avatar" class="avatar">
+  </div>
+  <br>
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+    <button type="submit">Login</button>
+    <hr>
+    <span class="psw"><a href="#">Don't have an account?</a></span>
+  </div>
+</form>
+</body>
+</html>
