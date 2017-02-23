6434372fab3869cff1e6bd7fb92dae862239b671
abidghozi <abidghozi.g@gmail.com>
2017-02-23 08:47:18 +0700

    yg diganti halaman index.php

--- Modul PHP Mysql/index.php
+++ Modul PHP Mysql/index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php include 'koneksi.php'; ?>
 <html>
 <head>
 	<title>Toko Online</title>
@@ -6,7 +7,7 @@
 </head>
 <body>
 	<?php
-		include 'navbar.php';
+	include 'navbar.php';
 	?>
 
 	<!-- Header -->
@@ -18,48 +19,58 @@
 	<!-- Sidebar -->
 	<div class="sidebar">
 		<table>
-			<tr><td><a href="#">Semua Kategori</a></td></tr>
-			<tr><td><a href="#">DVD</a></td></tr>
-			<tr><td><a href="#">Storage</a></td></tr>
-			<tr><td><a href="#">RAM</a></td></tr>
-			<tr><td><a href="#">CPU</a></td></tr>
+			<tr><td>Kategori Barang<hr></td></tr>
+			<?php
+			$sql = "SELECT * FROM tb_kategori_barang";
+			$result = mysqli_query($mysqli, $sql);
+			if (mysqli_num_rows($result) > 0) {
+				// output data of each row
+				while($row = mysqli_fetch_array($result)) {
+					//http://localhost/phpapsi/Modul%20PHP%20Mysql/index.php
+					$get_link = $link_server."phpapsi/Modul%20PHP%20Mysql/katalog.php?kategori_id=".$row[0];
+					echo "<tr><td><a href='".$get_link."'>".$row[1]."</a></td></tr>";
+				}
+			} else {
+				echo "0 results";
+			}
+			?>
 		</table>
 	</div>
 	<!-- End Sside -->
 
 	<!-- Konten -->
 	<div class="container">
-	<h3>Belanja dengan Kenikmatan</h3>
-		<div class="card">
-			<h4><b>Monitor</b></h4> 
-			<img src="">
-			<div class="content">
-				    <p>Harga :</p>
-				<button>Beli</button>
-			</div>
-		</div>
-		<div class="card">
-			<h4><b>RAM</b></h4> 
-			<img src="">
-			<div class="content">
-				    <p>Harga :</p>
-				<button>Beli</button>
-			</div>
-		</div>
-		<div class="card">
-			<h4><b>SSD</b></h4> 
-			<img src="">
-			<div class="content">
-				    <p>Harga :</p>
-				<button>Beli</button>
-			</div>
-		</div>
+		<h3>Belanja dengan Kenikmatan</h3>
+		<?php
+			$sql = "SELECT * FROM tb_barang ORDER BY kode_barang DESC LIMIT 3";
+			$result = mysqli_query($mysqli, $sql);
+			if (mysqli_num_rows($result) > 0) {
+				// output data of each row
+				while($row = mysqli_fetch_array($result)) {
+					//http://localhost/phpapsi/Modul%20PHP%20Mysql/index.php
+					$get_link = $link_server."phpapsi/Modul%20PHP%20Mysql/beli.php?id=".$row[1];
+					?>
+					<div class="card">
+						<h4><b><?php echo $row[2]; ?></b></h4>
+						<img src="">
+						<div class="content">
+							<p>Harga : <?php echo $row[3]; ?></p>
+							<button><a href="<?php echo $get_link; ?>">Beli</a></button>
+						</div>
+					</div>
+					<?php
+				}
+			} else {
+				echo "0 results";
+			}
+		?>
+		<a href='<?php echo $link_server."phpapsi/Modul%20PHP%20Mysql/katalog.php?"; ?>'>Buka Katalog</a>
 	</div>
 	<!-- End Konten -->
 
 	<!-- Footer -->
 	<?php
-		include 'footer.php';
+	include 'footer.php';
 	?>
 	<!-- End Footer -->
 </body>
--- Modul PHP Mysql/koneksi.php
+++ Modul PHP Mysql/koneksi.php
@@ -1,9 +1,12 @@
 <?php
+//Dipakai jika ingin memanggil default url
+$link_server = "http://$_SERVER[HTTP_HOST]";
+//=======================================
 $server="localhost";
 $username = "root";
 $password = "";
 $database = "db_toko_online";
-$mysqli = new \mysqli($server, $username, null, $database);
+$mysqli = new \mysqli($server, $username, $password, $database);
     if (\mysqli_connect_errno()) {
       echo"koneksi gagal";
     }else{
